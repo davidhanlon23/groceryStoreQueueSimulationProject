8dcdf8ae82c179cb6ae290f21409ad4a9aa53e4c
davidhanlon23 <32404063+davidhanlon23@users.noreply.github.com>
2018-04-25 13:37:11 -0400

    first commit

---
+++ src/cosc459_group_simulation/Driver.java
@@ -0,0 +1,18 @@
+/*=========================================================
+ Grocery Store Queue Simulation
+=========================================================
+ Project authors: David M. Hanlon, Rojaleen Chhetry,
+ Melvin Gaye, and Montrell Jubilee
+ COSC 459 - 101
+ Started: 04/15/2018
+--------------------------------------------------------*/
+package cosc459_group_simulation;
+import java.util.*;
+@SuppressWarnings("unused")
+
+public class Driver {
+	public static void main( String [] args) {
+		
+	}// end of main
+
+}//end of class
